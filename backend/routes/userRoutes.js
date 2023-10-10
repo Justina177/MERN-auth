8f2db5c98cbe2fdbcd386fe43ffdbd03a509bda2
justina177 <justinaenenche@yahoo.com>
2023-10-10 14:14:27 +0100

    Added login route function(authUser

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,7 +11,6 @@ import {
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
-router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
