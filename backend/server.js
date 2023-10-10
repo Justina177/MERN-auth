75f3660f8c2e17da9322867f2d0e9e59e407accb
justina177 <justinaenenche@yahoo.com>
2023-10-10 12:56:30 +0100

    Deed function to register user

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }))
+
 
 app.use('/api/users', userRoutes);
 
